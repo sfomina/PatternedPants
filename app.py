9cbd14de9633675f677738261d6d6cee04bd46d5
sfomina <afomina@stuy.edu>
2017-10-04 20:23:21 -0400

    almost

--- app.py
+++ app.py
@@ -2,21 +2,48 @@ from flask import Flask,render_template, request, session
 
 app = Flask (__name__)
 
+app.secret_key = 'secret'
+
 userCode = "sasha"
 passCode = "manahal"
+
+
 
 @app.route('/', methods = ['POST', 'GET'])
 
 def root():
-    return render_template('form.html')
+    if session.has_key('user'):
+        return render_template('welcome.html', name= session['user1'])
+    else:
+        return render_template('form.html')
 
 
 @app.route('/welcome', methods = ['POST', 'GET'])
 
 def welcome():
-    if request.form[username] == userCode and request.form[passCode] == passCode:    
-        return render_template(
+    
+    currentUser = request.form['username']
+    currentPass = request.form['password']
+                               
+    if currentUser  == userCode and currentPass == passCode:
+        session['user'] = currentUser
+        return render_template('welcome.html', name = currentUser)
 
+    
+    elif currentUser != userCode:  
+        return render_template('error.html', errorMsg = "Username is wrong!")
+
+    
+    else:
+        return render_template('error.html' , errorMsg = "Password is wrong!")
+
+@app.route('/logOut')
+
+def logOut():
+    #session.clear()
+    session.pop('user')
+    return render_template('logOut.html')
+    
 
 if __name__ == '__main__':
     app.run(debug= True)
